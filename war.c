0f3e1a77b8ad300d3fbb3bd7f4421f267aae9bcd
p4n1k0 <gabre.tarick@gmail.com>
2026-03-12 13:58:52 -0300

    estrutura para Territorio

--- war.c
+++ war.c
@@ -27,6 +27,11 @@
 
 // --- Estrutura de Dados ---
 // Define a estrutura para um território, contendo seu nome, a cor do exército que o domina e o número de tropas.
+struct Territorio {
+    char nome[TAM_NOME];
+    char cor[TAM_COR];
+    int tropas;
+};
 
 // --- Protótipos das Funções ---
 // Declarações antecipadas de todas as funções que serão usadas no programa, organizadas por categoria.
